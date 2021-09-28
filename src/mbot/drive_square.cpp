f9e76103bdfd80dc792e1162f0c1fb7891784a68
WuaoLiu <wuaoliu@umich.edu>
2021-09-28 16:13:02 -0400

    update to the report verision

--- src/mbot/drive_square.cpp
+++ src/mbot/drive_square.cpp
@@ -9,17 +9,17 @@
 int main(int argc, char** argv)
 {std::cout << "here" <<std::endl;
     int numTimes = 1;
-    
+
     if(argc > 1)
     {
         numTimes = std::atoi(argv[1]);
     }
-    
+
     std::cout << "Commanding robot to drive around 1m square " << numTimes << " times.\n";
-    
+
     robot_path_t path;
     path.path.resize(numTimes * 4);
-    
+
     pose_xyt_t nextPose;
 
 
@@ -31,7 +31,7 @@ int main(int argc, char** argv)
     {
         path.path[4*n] = nextPose;
     }
-    
+
     nextPose.x = 1.0f;
     nextPose.y = 1.0f;
     nextPose.theta = -M_PI;
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
     {
         path.path[4*n + 1] = nextPose;
     }
-    
+
     nextPose.x = 0.0f;
     nextPose.y = 1.0f;
     nextPose.theta = -M_PI_2;
@@ -47,7 +47,7 @@ int main(int argc, char** argv)
     {
         path.path[4*n + 2] = nextPose;
     }
-    
+
     nextPose.x = 0.0f;
     nextPose.y = 0.0f;
     nextPose.theta = 0.0f;
@@ -55,20 +55,20 @@ int main(int argc, char** argv)
     {
         path.path[4*n + 3] = nextPose;
     }
-    
+
     // Return to original heading after completing all circuits
 //    nextPose.theta = 0.0f;
 //    path.path.push_back(nextPose);
-    
+
     nextPose.x = 0.0f;
     nextPose.y = 0.0f;
     nextPose.theta = 0.0f;
     path.path.insert(path.path.begin(), nextPose);
-    
+
     path.path_length = path.path.size();
-    
+
     lcm::LCM lcmInstance(MULTICAST_URL);
-	std::cout << "publish to: " << CONTROLLER_PATH_CHANNEL << std::endl;
+    std::cout << "publish to: " << CONTROLLER_PATH_CHANNEL << std::endl;
     lcmInstance.publish(CONTROLLER_PATH_CHANNEL, &path);
     sleep(1);
 
--- src/mbot/motion_controller.cpp
+++ src/mbot/motion_controller.cpp
@@ -17,7 +17,7 @@
 #include <signal.h>
 #include "maneuver_controller.h"
 
-#define Vmax 0.25
+#define Vmax 0.75
 #define Wmax M_PI
 
 
@@ -25,7 +25,7 @@
 /**
  * Code below is a little more than a template. You will need
  * to update the maneuver controllers to function more effectively
- * and/or add different controllers. 
+ * and/or add different controllers.
  * You will at least want to:
  *  - Add a form of PID to control the speed at which your
  *      robot reaches its target pose.
@@ -39,33 +39,33 @@
 class StraightManeuverController : public ManeuverControllerBase
 {
 public:
-    StraightManeuverController() = default;   
+    StraightManeuverController() = default;
     virtual mbot_motor_command_t get_command(const pose_xyt_t& pose, const pose_xyt_t& target) override
     {
-    /**
-    * Send the command to go straight.
-    */
-    const float Kv = 0.8;
-    const float Kw = 1;
-    float dx = target.x - pose.x;
-    float dy = target.y - pose.y;
-    float d = sqrt(pow(dx, 2) + pow(dy, 2));
-    float alpha = angle_diff(atan2(dy, dx), pose.theta);
-    float v = Kv*d;
-    float w = Kw*alpha;
+        /**
+        * Send the command to go straight.
+        */
+        const float Kv = 1.0;
+        const float Kw = 1.2;
+        float dx = target.x - pose.x;
+        float dy = target.y - pose.y;
+        float d = sqrt(pow(dx, 2) + pow(dy, 2));
+        float alpha = angle_diff(atan2(dy, dx), pose.theta);
+        float v = Kv*d;
+        float w = Kw*alpha;
 
-    if(v>=Vmax){
-        v = Vmax;
-    }
-    if(w<-Wmax){
-        w = -Wmax;
-    }
-    else if(w>Wmax){
-        w = Wmax;
-    }
-    else{}
-    return {0, v, w};
-    //return {0, 0.25, 0};
+        if(v>=Vmax){
+            v = Vmax;
+        }
+
+        if(w<-Wmax){
+            w = -Wmax;
+        }
+        else if(w>Wmax){
+            w = Wmax;
+        }
+
+        return {0, v, w};
     }
 
     virtual bool target_reached(const pose_xyt_t& pose, const pose_xyt_t& target)  override
@@ -77,24 +77,25 @@ public:
 class TurnManeuverController : public ManeuverControllerBase
 {
 public:
-    TurnManeuverController() = default;   
+    TurnManeuverController() = default;
     virtual mbot_motor_command_t get_command(const pose_xyt_t& pose, const pose_xyt_t& target) override
     {
-    /**
-    * Send the command to turn.
-    */
-    const float Kw = 1;
-    float alpha = angle_diff(target.theta, pose.theta);
-    float w = Kw * alpha;
-    if(w<-Wmax){
-        w = -Wmax;
-    }
-    else if(w>Wmax){
-        w = Wmax;
-    }
-    else{}
-    return {0, 0, w};
-    //return {0, 0, M_PI/4};
+        /**
+        * Send the command to turn.
+        */
+        const float Kw = 2.0;
+        float beta = angle_diff(target.theta, pose.theta);
+        float w = Kw * beta;
+
+        if(w<-Wmax){
+            w = -Wmax;
+        }
+        else if(w>Wmax){
+            w = Wmax;
+        }
+
+        return {0, 0.0, w};
+        //return {0, 0, M_PI/4};
     }
 
     virtual bool target_reached(const pose_xyt_t& pose, const pose_xyt_t& target)  override
@@ -103,8 +104,10 @@ public:
         //float dy = target.y - pose.y;
         //float target_heading = atan2(dy, dx);
         //return (fabs(angle_diff(pose.theta, target_heading)) < 0.07);
+
+        // only consider the orientation but do not care the position
         return (fabs(angle_diff(pose.theta, target.theta)) < 0.07);
-        //only consider the orientation but do not care the position
+
 
     }
 };
@@ -118,20 +121,21 @@ public:
         /**
         * Send the command to turn.
         */
-        const float Kw = 1;
+        const float Kw = 2.0;
         float dx = target.x - pose.x;
         float dy = target.y - pose.y;
         float target_heading = atan2(dy, dx);
         float alpha = angle_diff(target_heading, pose.theta);
         float w = Kw * alpha;
+
         if(w<-Wmax){
             w = -Wmax;
         }
         else if(w>Wmax){
             w = Wmax;
         }
-        else{}
-        return {0, 0, w};
+
+        return {0, 0.0, w};
         //return {0, 0, M_PI/4};
     }
 
@@ -140,42 +144,41 @@ public:
         float dx = target.x - pose.x;
         float dy = target.y - pose.y;
         float target_heading = atan2(dy, dx);
-        return (fabs(angle_diff(target_heading, pose.theta)) < 0.05);
+        return (fabs(angle_diff(target_heading, pose.theta)) < 0.10);
     }
 };
 
 
 class MotionController
-{ 
-public: 
-    
+{
+public:
+
     /**
     * Constructor for MotionController.
     */
     MotionController(lcm::LCM * instance)
-    :
-        lcmInstance(instance),
-        odomToGlobalFrame_{0, 0, 0, 0}
+            :
+            lcmInstance(instance),
+            odomToGlobalFrame_{0, 0, 0, 0}
     {
         subscribeToLcm();
 
-	    time_offset = 0;
-	    timesync_initialized_ = false;
-    } 
-    
+        time_offset = 0;
+        timesync_initialized_ = false;
+    }
+
     /**
     * \brief updateCommand calculates the new motor command to send to the Mbot. This method is called after each call to
     * lcm.handle. You need to check if you have sufficient data to calculate a new command, or if the previous command
     * should just be used again until for feedback becomes available.
-    * 
+    *
     * \return   The motor command to send to the mbot_driver.
     */
     mbot_motor_command_t updateCommand(void)
     {
         mbot_motor_command_t cmd {now(), 0.0, 0.0};
-        
-        if(!targets_.empty() && !odomTrace_.empty()) 
-        {
+
+        if(!targets_.empty() && !odomTrace_.empty()){
             pose_xyt_t target = targets_.back();
             pose_xyt_t pose = currentPose();
 
@@ -183,28 +186,33 @@ public:
             if (state_ == Rotate){
                 if(rotate_controller.target_reached(pose, target))
                 {
+                    std::cout << "Enter DRIVE state.\n";
+                    cmd.trans_v = 0.0;
+                    cmd.angular_v = 0.0;
                     state_ = DRIVE;
                 }
                 else
                 {
                     cmd = rotate_controller.get_command(pose, target);
+                    cmd.utime = now();
                 }
             }
             else if(state_ == TURN)
-            { 
+            {
                 if(turn_controller.target_reached(pose, target))
                 {
-		            if(!assignNextTarget())
+                    if(!assignNextTarget())
                     {
-                      std::cout<<"\\rTarget Reached!";
+                        std::cout<<"\\rTarget Reached!";
                     }
-                } 
+                }
                 else
                 {
                     cmd = turn_controller.get_command(pose, target);
+                    cmd.utime = now();
                 }
             }
-            else if(state_ == DRIVE) 
+            else if(state_ == DRIVE)
             {
                 if(straight_controller.target_reached(pose, target))
                 {
@@ -212,42 +220,43 @@ public:
 
                     //if(!assignNextTarget())
                     //{
-                        //std::cout << "\rTarget Reached!";
+                    //std::cout << "\rTarget Reached!";
                     //}
                 }
                 else
-                { 
+                {
                     cmd = straight_controller.get_command(pose, target);
+                    cmd.utime = now();
                 }
-		    }
+            }
             else
             {
                 std::cerr << "ERROR: MotionController: Entered unknown state: " << state_ << '\n';
             }
-            cmd = {now(), cmd.trans_v, cmd.angular_v};
+            //cmd = {now(), cmd.trans_v, cmd.angular_v};
             //std::cout<<"State:"<<state_<<" "<<"Pose:"<<"("<<pose.x<<","<<pose.y<<","<<pose.theta<<")"<<" "<<"Tar:"<<"("<<target.x<<","<<target.y<<","<<target.theta<<")"<<"\n";
-		}
-        return cmd; 
+        }
+        return cmd;
     }
 
     bool timesync_initialized(){ return timesync_initialized_; }
 
     void handleTimesync(const lcm::ReceiveBuffer* buf, const std::string& channel, const timestamp_t* timesync)
     {
-	    timesync_initialized_ = true;
-	    time_offset = timesync->utime-utime_now();
+        timesync_initialized_ = true;
+        time_offset = timesync->utime-utime_now();
     }
-    
+
     void handlePath(const lcm::ReceiveBuffer* buf, const std::string& channel, const robot_path_t* path)
     {
         targets_ = path->path;
         std::reverse(targets_.begin(), targets_.end()); // store first at back to allow for easy pop_back()
 
-    	std::cout << "received new path at time: " << path->utime << "\n"; 
-    	for(auto pose : targets_)
+        std::cout << "received new path at time: " << path->utime << "\n";
+        for(auto pose : targets_)
         {
-    		std::cout << "(" << pose.x << "," << pose.y << "," << pose.theta << "); ";
-    	}
+            std::cout << "(" << pose.x << "," << pose.y << "," << pose.theta << "); ";
+        }
         std::cout << std::endl;
 
         assignNextTarget();
@@ -256,20 +265,20 @@ public:
         message_received_t confirm {now(), path->utime, channel};
         lcmInstance->publish(MESSAGE_CONFIRMATION_CHANNEL, &confirm);
     }
-    
+
     void handleOdometry(const lcm::ReceiveBuffer* buf, const std::string& channel, const odometry_t* odometry)
     {
         pose_xyt_t pose {odometry->utime, odometry->x, odometry->y, odometry->theta};
         odomTrace_.addPose(pose);
     }
-    
+
     void handlePose(const lcm::ReceiveBuffer* buf, const std::string& channel, const pose_xyt_t* pose)
     {
         computeOdometryOffset(*pose);
     }
-    
+
 private:
-    
+
     enum State
     {
         Rotate,
@@ -277,7 +286,7 @@ private:
         DRIVE,
 
     };
-    
+
     pose_xyt_t odomToGlobalFrame_;      // transform to convert odometry into the global/map coordinates for navigating in a map
     PoseTrace  odomTrace_;              // trace of odometry for maintaining the offset estimate
     std::vector<pose_xyt_t> targets_;
@@ -288,47 +297,47 @@ private:
     bool timesync_initialized_;
 
     lcm::LCM * lcmInstance;
- 
+
     TurnManeuverController turn_controller;
     StraightManeuverController straight_controller;
     RotateManeuverController rotate_controller;
 
     int64_t now()
     {
-	    return utime_now() + time_offset;
+        return utime_now() + time_offset;
     }
-    
+
     bool assignNextTarget(void)
     {
         if(!targets_.empty()) { targets_.pop_back(); }
         state_ = Rotate;
         return !targets_.empty();
     }
-    
+
     void computeOdometryOffset(const pose_xyt_t& globalPose)
     {
         pose_xyt_t odomAtTime = odomTrace_.poseAt(globalPose.utime);
         double deltaTheta = globalPose.theta - odomAtTime.theta;
         double xOdomRotated = (odomAtTime.x * std::cos(deltaTheta)) - (odomAtTime.y * std::sin(deltaTheta));
         double yOdomRotated = (odomAtTime.x * std::sin(deltaTheta)) + (odomAtTime.y * std::cos(deltaTheta));
-         
+
         odomToGlobalFrame_.x = globalPose.x - xOdomRotated;
-        odomToGlobalFrame_.y = globalPose.y - yOdomRotated; 
+        odomToGlobalFrame_.y = globalPose.y - yOdomRotated;
         odomToGlobalFrame_.theta = deltaTheta;
     }
-    
+
     pose_xyt_t currentPose(void)
     {
         assert(!odomTrace_.empty());
-        
+
         pose_xyt_t odomPose = odomTrace_.back();
         pose_xyt_t pose;
-        pose.x = (odomPose.x * std::cos(odomToGlobalFrame_.theta)) - (odomPose.y * std::sin(odomToGlobalFrame_.theta)) 
-            + odomToGlobalFrame_.x;
+        pose.x = (odomPose.x * std::cos(odomToGlobalFrame_.theta)) - (odomPose.y * std::sin(odomToGlobalFrame_.theta))
+                 + odomToGlobalFrame_.x;
         pose.y = (odomPose.x * std::sin(odomToGlobalFrame_.theta)) + (odomPose.y * std::cos(odomToGlobalFrame_.theta))
-            + odomToGlobalFrame_.y;
+                 + odomToGlobalFrame_.y;
         pose.theta = angle_sum(odomPose.theta, odomToGlobalFrame_.theta);
-        
+
         return pose;
     }
 
@@ -347,16 +356,16 @@ int main(int argc, char** argv)
     MotionController controller(&lcmInstance);
 
     signal(SIGINT, exit);
-    
+
     while(true)
     {
         lcmInstance.handleTimeout(50);  // update at 20Hz minimum
 
-    	if(controller.timesync_initialized()){
-            	mbot_motor_command_t cmd = controller.updateCommand();
-            	lcmInstance.publish(MBOT_MOTOR_COMMAND_CHANNEL, &cmd);
-    	}
+        if(controller.timesync_initialized()){
+            mbot_motor_command_t cmd = controller.updateCommand();
+            lcmInstance.publish(MBOT_MOTOR_COMMAND_CHANNEL, &cmd);
+        }
     }
-    
+
     return 0;
 }
