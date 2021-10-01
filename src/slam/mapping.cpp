f5994e06fa5243b87f6de2841958bab1547b67d0
WuaoLiu <wuaoliu@umich.edu>
2021-09-30 22:47:03 -0400

    from live code, have not test

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -16,4 +16,51 @@ Mapping::Mapping(float maxLaserDistance, int8_t hitOdds, int8_t missOdds)
 void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGrid& map)
 {
     //////////////// TODO: Implement your occupancy grid algorithm here ///////////////////////
+    if(!initialized_){
+        previousPose_ = pose;
+    }
+    initialized = true;
+    MovingLaserScan movingScan(scan, previousPose_, pose);
+
+    for(auto& ray: movingScan){
+        scoreEndpoint(ray, map);
+        scoreRay(ray, map);
+    }
+
+    previousPose_ = pose;
 }
+
+void Mapping::scoreEndpoint(const adjusted_ray_t& ray, OccupancyGrid& map){
+    if(ray.range < kMaxLaserDistance_){
+        Point<float> rayStart = global_position_to_grid_cell(ray.origin, map);
+        Point<int> rayCell;
+
+        rayCell.x = static_cast<int>((ray.range * std::cos(ray.theta) * map.cellsPerMeter()) + rayStart.x);
+        rayCell.y = static_cast<int>((ray.range * std::sin(ray.theta) * map.cellsPerMeter()) + rayStart.y);
+
+        if(map.isCellInGrid(rayCell.x, rayCell.y)){
+            increaseCellOdds(rayCell.x, rayCell.y, map);
+        }
+
+    }
+}
+void Mapping::scoreRay(const adjusted_ray_t& ray, OccupancyGrid& map){
+
+}
+void Mapping::decreaseCellOdds(int x, int y, OccupancyGrid& map){
+
+}
+void Mapping::increaseCellOdds(int x, int y, OccupancyGrid& map){
+    //make sure we would not overflow;
+    //1.check overflow
+    //
+    if(std::numeric_limits<CellOdds>::max() - map(x,y) > kHitOdds_){
+        map(x, y)+= kHitOdds_;
+    }
+    else{
+        map(x, y) = std::numeric_limits<CellOdds>::max();
+    }
+
+}
+
+
