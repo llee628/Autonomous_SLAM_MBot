9f2f033d64562cdd0bf832e8ec7d80a30f2946e8
WuaoLiu <wuaoliu@umich.edu>
2021-10-05 15:19:28 -0400

    finish the mapping part!

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -24,7 +24,7 @@ void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGr
 
     for(auto& ray: movingScan){
         scoreEndpoint(ray, map);
-        //scoreRay(ray, map);
+        scoreRay(ray, map);
     }
 
     previousPose_ = pose;
@@ -46,6 +46,50 @@ void Mapping::scoreEndpoint(const adjusted_ray_t& ray, OccupancyGrid& map){
 }
 void Mapping::scoreRay(const adjusted_ray_t& ray, OccupancyGrid& map){
     // TODO: implement the Breshenham’s line algorithm
+    if(ray.range < kMaxLaserDistance_){
+        Point<float> rayStart = global_position_to_grid_cell(ray.origin, map);
+        Point<int> rayOrigin;
+        //start point of this beam
+        Point<int> rayCell;
+        //end point of this beam
+        rayOrigin.x = static_cast<int>(rayStart.x);
+        rayOrigin.y = static_cast<int>(rayStart.y);
+        rayCell.x = static_cast<int>((ray.range * std::cos(ray.theta) * map.cellsPerMeter()) + rayStart.x);
+        rayCell.y = static_cast<int>((ray.range * std::sin(ray.theta) * map.cellsPerMeter()) + rayStart.y);
+        int dx = std::abs(rayCell.x - rayOrigin.x);
+        int dy = std::abs(rayCell.y - rayOrigin.y);
+        int sx, sy;
+        int x, y, err, e2;
+        // the step to take
+        if(rayOrigin.x<rayCell.x){
+            sx = 1;
+        }
+        else{
+            sx = -1;
+        }
+        if(rayOrigin.y<rayCell.y){
+            sy = 1;
+        }
+        else{
+            sy = -1;
+        }
+        err = dx - dy;
+        x = rayOrigin.x;
+        y = rayOrigin.y;
+        while(x!=rayCell.x||y!=rayCell.y){
+            decreaseCellOdds(x, y, map);
+            e2 = 2*err;
+            if(e2>=-dy){
+                err -= dy;
+                x += sx;
+            }
+            if(e2<=dx){
+                err += dx;
+                y += sy;
+            }
+        }
+
+    }
 
 }
 void Mapping::decreaseCellOdds(int x, int y, OccupancyGrid& map){
